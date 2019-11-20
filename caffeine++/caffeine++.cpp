d6a6cfcfc6d197e947b5023fb91d6b44e2c68ceb
Xaris Ar <xaris@tuta.io>
2019-11-20 16:03:52 +0200

    Added timer functionality

--- caffeine++/caffeine++.cpp
+++ caffeine++/caffeine++.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <Windows.h>
 #include <signal.h>
+#include <time.h>
 
 using namespace std;
 
@@ -17,13 +18,18 @@ void signal_handler(int signum) {
 
 int main(int argc, char *argv[], char *envp[])
 {
-	int i;
-	if (argc > 3)
+	//#define NUM_SECS 1.0f // delay period
+	bool control_switch = 1;
+	time_t start, end;
+	start = time(NULL);
+	double elapsed; //seconds
+	double timer=0;
+	if (argc > 4)
 	{
 		cout << "Wrong number of arguments.";
 		return 0;
 	}
-	else if (argc > 1 and argc <= 3)
+	else if (argc > 1 and argc <= 4)
 	{
 		for (int i = 1; i < argc; ++i)
 		{
@@ -34,24 +40,43 @@ int main(int argc, char *argv[], char *envp[])
 			else if (argv[i] == string("-t") || argv[i] == string("-timer"))
 			{
 				//TODO Implement Timer function
-				cout << "Running the -t hand\n";
+				cout << "\nRunning the -t hand";
+				string input = argv[i + 1];
+				timer = atof(input.c_str());
+				i++;
 			}
 			else
 			{
-				cout << "Invalid argument " << argv[i] <<", please try again.\n";
+				cout << "\rInvalid argument " << argv[i] <<", please try again.\n";
 				ShowWindow(GetConsoleWindow(), SW_NORMAL);
 				Sleep(1500);
-				exit(1);
+				return EXIT_FAILURE;
 			}
 			//Debug
 			//cout << "\nArgc is \"" << argc << "\"" << "\nArgv is :\"" << argv[1] << "\"\n" << (argv[1] == "-m" ? "true" : "false")<<"\n";
 		}
 	}
-
+	// Hide Cursor from cmd
+	HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO     cursorInfo;
+	GetConsoleCursorInfo(out, &cursorInfo);
+	cursorInfo.bVisible = false;
+	SetConsoleCursorInfo(out, &cursorInfo);
+	
+	// Add signal interupt
 	signal(SIGINT, signal_handler);
+
+	//Inform user
 	cout << "Caffeine++ is runnning\n";
 	cout << "Use Control-C to stop this service and restore previous state\n";
-	while (1) {
+	
+	// Run Caffeine++
+	while (control_switch) {
+		end = time(NULL);
+		elapsed = difftime(end, start);
+		cout << "\rElapsed " << elapsed << " seconds";
+		if (elapsed >= timer && timer >0 )
+			control_switch = 0;
 		SetThreadExecutionState(ES_CONTINUOUS | ES_DISPLAY_REQUIRED | ES_SYSTEM_REQUIRED | ES_AWAYMODE_REQUIRED);
 	}
 	return EXIT_SUCCESS;
